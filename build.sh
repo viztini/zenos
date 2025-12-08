7d5441b2e658a5df0601c43f2dae70dd9e797dab
viztini <leon.stonehouseburguillo@education.nsw.gov.au>
2025-12-09 10:49:14 +1100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ sh /home/build/aports/scripts/mkimage.sh --tag edge \
 	--arch x86_64 \
 	--repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
 	--extra-repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
-	--profile saigo
+	--profile zenos
